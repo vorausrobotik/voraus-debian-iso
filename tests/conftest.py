7c5e1744cce305d4316db55756fd1bb48ed4b841
Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>
2025-08-28 13:32:46 +0200

    tests: Run integration test on generated ISO
    Note that the tests are only executed on Linux machines because QEMU is required.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,21 @@
 """This module contains pytest fixtures."""
 
+from __future__ import annotations
+
+import logging
+import os
 from pathlib import Path
+from typing import Generator
 
 import pytest
+from fabric import Connection
+from typer.testing import CliRunner
+
+from voraus_debian_iso.cli.main import app
+from voraus_debian_iso.constants import DEFAULT_QEMU_DISK_FILE
+from voraus_debian_iso.methods.cli.cli_start_methods import get_ssh_connection
+
+_logger = logging.getLogger(__file__)
 
 
 @pytest.fixture(scope="session", name="resource_dir")
@@ -13,3 +26,36 @@ def resource_dir_fixture() -> Path:
         Path: The resource directory path.
     """
     return Path(__file__).parent.joinpath("resources")
+
+
+@pytest.fixture(scope="session", name="cli_runner")
+def cli_runner_fixture() -> CliRunner:
+    """Returns a typer/click CliRunner with increased terminal width.
+
+    Returns:
+        The CliRunner object for testing the CLI.
+    """
+    return CliRunner(env={"COLUMNS": "120"})
+
+
+@pytest.fixture(scope="session", name="dut")
+def device_under_test_fixture(cli_runner: CliRunner) -> Generator[Connection, None, None]:
+    images_path = Path(__file__).parent.parent / "output"
+    iso_files = list(images_path.glob("*.iso"))
+    match len(iso_files):
+        case 0:
+            raise RuntimeError(f"No ISO file found in {images_path}")
+        case 1:
+            iso_path = images_path / iso_files[0]
+        case _:
+            iso_path = max(iso_files, key=os.path.getctime)
+            _logger.warning(f"Multiple ISO files found in {images_path}: {iso_files}. Using latest ISO file {iso_path}")
+
+    if not DEFAULT_QEMU_DISK_FILE.is_file():
+        cli_runner.invoke(app, ["install", "--iso-file", str(iso_path), "--disk-file", str(DEFAULT_QEMU_DISK_FILE)])
+
+    cli_runner.invoke(app, ["start", "--disk-file", str(DEFAULT_QEMU_DISK_FILE)])
+
+    yield from get_ssh_connection()
+
+    cli_runner.invoke(app, ["stop"])
---
+++ tests/integration/test_iso.py
@@ -0,0 +1,20 @@
+"""Contains all tests for the ISO."""
+
+import sys
+
+import pytest
+from fabric import Connection
+
+
+@pytest.mark.skipif(sys.platform != "linux", reason="Only supported on linux because it requires qemu")
+class TestISO:
+    """Contains all ISO tests."""
+
+    def test_debian_version(self, dut: Connection) -> None:
+        assert dut.run("cat /etc/debian_version").stdout.strip() == "13.0"
+
+    def test_debian_kernel(self, dut: Connection) -> None:
+        assert dut.run("uname -r").stdout.strip() == "6.12.41+deb13-amd64"
+
+    def test_python_version(self, dut: Connection) -> None:
+        assert dut.run("python3 --version").stdout.strip() == "Python 3.13.5"

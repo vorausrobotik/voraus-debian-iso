3a2a2c7caa972db279aa9af6213292c9478417a7
Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>
2025-09-09 13:18:12 +0200

    feat(ssh): Enable root SSH access with password
    This is required for configuring the system via Ansible.

--- tests/integration/test_iso.py
+++ tests/integration/test_iso.py
@@ -5,6 +5,8 @@ import sys
 import pytest
 from fabric import Connection
 
+from voraus_debian_iso.methods.cli.cli_start_methods import get_ssh_connection
+
 
 @pytest.mark.skipif(sys.platform != "linux", reason="Only supported on linux because it requires qemu")
 class TestISO:
@@ -18,3 +20,7 @@ class TestISO:
 
     def test_python_version(self, dut: Connection) -> None:
         assert dut.run("python3 --version").stdout.strip() == "Python 3.13.5"
+
+    def test_root_ssh_access(self, dut: Connection) -> None:  # pylint: disable=unused-argument
+        root_ssh_connection = next(get_ssh_connection(username="root"))
+        assert root_ssh_connection.run("whoami").stdout.strip() == "root"

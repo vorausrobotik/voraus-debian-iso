142e50f1feae74a82fae7275f0f8b9b7cb56c280
Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>
2025-11-20 09:09:22 +0100

    feat: Bump base debian to 13.2
    Release notes: https://www.debian.org/News/2025/20251115

--- tests/integration/test_iso.py
+++ tests/integration/test_iso.py
@@ -13,10 +13,10 @@ class TestISO:
     """Contains all ISO tests."""
 
     def test_debian_version(self, dut: Connection) -> None:
-        assert dut.run("cat /etc/debian_version").stdout.strip() == "13.1"
+        assert dut.run("cat /etc/debian_version").stdout.strip() == "13.2"
 
     def test_debian_kernel(self, dut: Connection) -> None:
-        assert dut.run("uname -r").stdout.strip() == "6.12.43+deb13-amd64"
+        assert dut.run("uname -r").stdout.strip() == "6.12.57+deb13-amd64"
 
     def test_python_version(self, dut: Connection) -> None:
         assert dut.run("python3 --version").stdout.strip() == "Python 3.13.5"
